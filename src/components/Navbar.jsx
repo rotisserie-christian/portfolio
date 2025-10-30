ad98c9fac1f81ddbd6c24c7368aa01fd681e2a37
worldofchristian <christianwaters@protonmail.com>
2025-10-30 01:15:29 -0600

    added tiktok to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaTiktok } from "react-icons/fa";
 
 export default function Navbar() {
     return (
         <nav className="sticky top-0 z-50 backdrop-blur-sm w-full bg-base-100/80 shadow-md">
-            <div className="flex items-center justify-center w-full">
-                <div className="flex flex-row items-center justify-between w-full max-w-7xl py-2 px-4">
+            <div className="flex items-center justify-between w-full">
+                <div className="flex flex-row items-center justify-between w-full max-w-7xl mx-auto py-2 px-4">
                     <a href='https://github.com/rotisserie-christian' target='_blank' rel='noreferrer'>
                         <button 
                         className="btn btn-neutral rounded-xl text-neutral-content/85">
@@ -12,12 +12,21 @@ export default function Navbar() {
                         </button>
                     </a>
 
-                    <a href='https://linkedin.com/in/cwaters123' target='_blank' rel='noreferrer'>
-                        <button 
-                        className="btn btn-neutral rounded-xl text-neutral-content/85">
-                            <FaLinkedin />Contact
-                        </button>
-                    </a>
+                    <div className="flex flex-row items-center gap-2">
+                        <a href='https://www.tiktok.com/@fakeairlines.com' target='_blank' rel='noreferrer'>
+                            <button 
+                            className="btn btn-neutral rounded-xl text-neutral-content/85">
+                                <FaTiktok />TikTok
+                            </button>
+                        </a>
+                        
+                        <a href='https://linkedin.com/in/cwaters123' target='_blank' rel='noreferrer'>
+                            <button 
+                            className="btn btn-neutral rounded-xl text-neutral-content/85">
+                                <FaLinkedin />Contact
+                            </button>
+                        </a>
+                    </div>
                 </div>
             </div>
         </nav>
