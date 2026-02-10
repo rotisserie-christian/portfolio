6453dd538f11b473440156f35d1f91c1a3e307dd
worldofchristian <christianwaters@protonmail.com>
2026-02-10 02:20:33 -0600

    created new feature directory for search profiler

---
+++ src/components/searchprofiler/SearchProfiler.jsx
@@ -0,0 +1,30 @@
+import { FaAngleDoubleRight, FaPython } from "react-icons/fa";
+
+export default function SearchProfiler() {
+    return (
+        <section className="flex items-center justify-center w-full bg-base-300">
+            <div className="flex flex-col mt-20 mb-10 lg:mb-16 items-center justify-center w-full">
+                <h1 className="ubuntu-bold text-3xl lg:text-5xl text-neutral-content/85">
+                    Search Profiler
+                </h1>
+
+                <div className="flex flex-row items-center justify-center w-24 h-8 bg-base-100 rounded-lg gap-2 mt-4">
+                    <FaPython className="text-xl text-neutral-content/85" />
+                    <p className="text-xs text-neutral-content/85">Python</p>
+                </div>
+
+                <p className="text-lg lg:text-xl mt-4 lg:mb-4 text-neutral-content/85 text-center max-w-xs lg:max-w-lg">
+                    Toolkit for researching search terms specific to the behaviour of a given user profile
+                </p>
+
+                <a
+                    href='https://github.com/rotisserie-christian/search-profiler'
+                    target='_blank' rel='noreferrer'>
+                    <button className="btn w-26 bg-neutral rounded-xl mt-4 mb-8">
+                        GitHub<FaAngleDoubleRight />
+                    </button>
+                </a>
+            </div>
+        </section>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 import { ShootingStars } from "../components/starfield/ShootingStars";
 import { StarsBackground } from "../components/starfield/StarsBackground";
 import Crayonbrain from "../components/crayonbrain/Crayonbrain";
+import SearchProfiler from "../components/searchprofiler/SearchProfiler";
 
 export default function Home() {
     const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
     const scrollToProjects = () => {
-        document.querySelector('[data-section="crayonbrain"]')?.scrollIntoView({ 
+        document.querySelector('[data-section="crayonbrain"]')?.scrollIntoView({
             behavior: 'smooth',
             block: 'start'
         });
@@ -15,33 +16,37 @@ export default function Home() {
 
     return (
         <>
-        <div className="flex flex-col items-center justify-center w-full">
-          <section className="flex items-center justify-center w-full min-h-screen bg-base-300 min-h-screen">
-            <StarsBackground />
-            <ShootingStars />
+            <div className="flex flex-col items-center justify-center w-full">
+                <section className="flex items-center justify-center w-full min-h-screen bg-base-300 min-h-screen">
+                    <StarsBackground />
+                    <ShootingStars />
 
-              <div className="flex flex-col items-center justify-center bg-base-300/60 mb-20 lg:mb-0 z-40 lg:px-16 rounded-3xl">
-                  <h1 className="text-3xl lg:text-5xl text-neutral-content/85 ubuntu-bold">
-                      Christian Waters
-                  </h1>
+                    <div className="flex flex-col items-center justify-center bg-base-300/60 mb-20 lg:mb-0 z-40 lg:px-16 rounded-3xl">
+                        <h1 className="text-3xl lg:text-5xl text-neutral-content/85 ubuntu-bold">
+                            Christian Waters
+                        </h1>
 
-                  <p className="text-lg lg:text-xl mt-4 lg:mb-4 text-neutral-content/85 text-center max-w-xs lg:max-w-lg">
-                      {!isChrome && <span className="text-xl mr-2">🇨🇦</span>}Web Developer 
-                  </p>
+                        <p className="text-lg lg:text-xl mt-4 lg:mb-4 text-neutral-content/85 text-center max-w-xs lg:max-w-lg">
+                            {!isChrome && <span className="text-xl mr-2">🇨🇦</span>}Web Developer
+                        </p>
 
-                  <button 
-                      onClick={scrollToProjects}
-                      className="btn w-28 lg:w-32 lg:btn-lg btn-neutral text-cyan-200 rounded-xl lg:rounded-2xl mt-10"
-                  >
-                      Projects<FaAngleDoubleRight />
-                  </button>
-              </div>
-          </section>
+                        <button
+                            onClick={scrollToProjects}
+                            className="btn w-28 lg:w-32 lg:btn-lg btn-neutral text-cyan-200 rounded-xl lg:rounded-2xl mt-10"
+                        >
+                            Projects<FaAngleDoubleRight />
+                        </button>
+                    </div>
+                </section>
 
-          <div data-section="crayonbrain" className="w-full">
-            <Crayonbrain />
-          </div>
-        </div>
+                <div data-section="crayonbrain" className="w-full">
+                    <Crayonbrain />
+                </div>
+
+                <div data-section="searchprofiler" className="w-full">
+                    <SearchProfiler />
+                </div>
+            </div>
         </>
     );
 }
