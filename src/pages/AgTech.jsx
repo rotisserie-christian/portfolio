297092f7fc7953932c955a87a6564d1ab4d836cc
worldofchristian <christianwaters@protonmail.com>
2026-04-04 05:15:46 -0600

    added increment/decrement buttons to agtech widget

--- src/pages/AgTech.jsx
+++ src/pages/AgTech.jsx
@@ -1,4 +1,3 @@
-//import { FaMicrochip, FaChartLine, FaPuzzlePiece, FaAngleDoubleRight } from "react-icons/fa";
 import Contact from "../components/contact/Contact";
 import Footer from "../components/ui/Footer";
 import SpoilageWidget from "../components/agtech/SpoilageWidget";
@@ -6,7 +5,6 @@ import SpoilageWidget from "../components/agtech/SpoilageWidget";
 export default function AgTech() {
     return (
         <div className="flex flex-col items-center justify-center w-full min-h-screen bg-base-300">
-            {/* Embedded Widget Interactive Example */}
             <section className="w-full bg-base-200 py-32 px-6 flex flex-col items-center">
                 <div className="max-w-4xl text-center mb-16">
                     <h2 className="text-4xl lg:text-5xl font-bold mb-6 ubuntu-bold text-neutral-content/85">Widgeto</h2>
@@ -18,7 +16,6 @@ export default function AgTech() {
                 <SpoilageWidget />
             </section>
 
-            {/* Reused Contact Section */}
             <div className="w-full bg-base-200">
                 <Contact />
             </div>
