314b0056f86c3c8b38767c5cbaf058bd68758acc
worldofchristian <christianwaters@protonmail.com>
2026-06-24 16:56:47 -0600

    rm agtech

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '@/App';
 import Home from '@/pages/Home';
-import AgTech from '@/pages/AgTech';
 import ErrorBoundary from '@/components/ui/ErrorBoundary';
 import Temp from './pages/Temp';
 
@@ -13,7 +12,6 @@ const RouteSwitch = () => {
                     <Route path="/" element={<App />}>
                         <Route index element={<Home />} />
                         <Route path="semanticmaps" element={<Home />} />
-                        <Route path="agtech" element={<AgTech />} />
                         <Route path="temp" element={<Temp />} />
                     </Route>
                 </Routes>
